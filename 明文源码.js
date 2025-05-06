0030916e05b999fc6221d898723d939ba60e1172
micei1 <122587845+micei1@users.noreply.github.com>
2025-05-06 04:36:06 -0900

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = 'e7bbe037-3c11-4335-9bc8-d79074c5ebd3';
 let proxyIP = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
